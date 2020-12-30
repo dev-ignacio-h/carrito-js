d8e513355475520f7d98affafdd1120fbcd2a8de
Ignacio <ignacio.henriquez@evalueserve.com>
2020-12-30 00:29:17 -0300

    creacion del objeto curso

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// variables
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+
+function cargarEventListeners() {
+    listaCursos.addEventListener('click', agregarCurso); // al presionar "agregar al carrito"
+}
+
+// funciones
+function agregarCurso(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')) {
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+    }
+}
+
+// leer contenido del html donde se dio click y extraer la info del curso
+function leerDatosCurso(curso) {
+    // crear objeto con contenido de curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    console.log(infoCurso);
+}
